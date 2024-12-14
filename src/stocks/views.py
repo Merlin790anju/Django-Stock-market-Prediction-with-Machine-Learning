c99c9a3000dee8b80ba4e516a303fbfe245e8292
Merlin790anju <merlinsara05@gmail.com>
2024-12-14 05:38:45 +0530

    display ticker info

--- src/stocks/views.py
+++ src/stocks/views.py
@@ -220,3 +220,25 @@ def predict(request, ticker_value, number_of_days):
     pred_fig.update_xaxes(rangeslider_visible=True)
     pred_fig.update_layout(paper_bgcolor="#14151b", plot_bgcolor="#14151b", font_color="white")
     plot_div_pred = plot(pred_fig, auto_open=False, output_type='div')
+
+    # ========================================== Display Ticker Info ==========================================
+
+    ticker = pd.read_csv('app/Data/Tickers.csv')
+    to_search = ticker_value
+    ticker.columns = ['Symbol', 'Name', 'Last_Sale', 'Net_Change', 'Percent_Change', 'Market_Cap',
+                    'Country', 'IPO_Year', 'Volume', 'Sector', 'Industry']
+    for i in range(0,ticker.shape[0]):
+        if ticker.Symbol[i] == to_search:
+            Symbol = ticker.Symbol[i]
+            Name = ticker.Name[i]
+            Last_Sale = ticker.Last_Sale[i]
+            Net_Change = ticker.Net_Change[i]
+            Percent_Change = ticker.Percent_Change[i]
+            Market_Cap = ticker.Market_Cap[i]
+            Country = ticker.Country[i]
+            IPO_Year = ticker.IPO_Year[i]
+            Volume = ticker.Volume[i]
+            Sector = ticker.Sector[i]
+            Industry = ticker.Industry[i]
+            break
+
