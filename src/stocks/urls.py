35602126fbe595d415df543558bc6314971d7453
Merlin790anju <merlinsara05@gmail.com>
2024-12-26 14:06:55 +0530

    url patterns

--- src/stocks/urls.py
+++ src/stocks/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from stocks.views import *
 
 urlpatterns = [
-    
+    path('', index),
+    path('search/', search),
+    path('predict/<str:ticker_value>/<str:number_of_days>/', predict),
+    path('ticker/', ticker),
 ]
